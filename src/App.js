eca6306642e95f2d9cc5a828406e6789460c9600
domeneack <112799387+domeneack@users.noreply.github.com>
2023-04-24 12:11:53 -0700

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,20 +3,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a href="https://tangerine-moonbeam-52db1f.netlify.app/" >
-          Domeneack Foster
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/domeneack/react-weather-app"
-          
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a href="https://tangerine-moonbeam-52db1f.netlify.app/">
+            Domeneack Foster
+          </a>{" "}
+          and is{" "}
+          <a href="https://github.com/domeneack/react-weather-app">
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
