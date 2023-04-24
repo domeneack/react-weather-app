17e17088cad54c86b1a9d1ffc11572e0ebd13e0e
domeneack <112799387+domeneack@users.noreply.github.com>
2023-04-24 11:08:18 -0700

    Added the GitHub repo url

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a href="https://tangerine-moonbeam-52db1f.netlify.app/" target="_blank">
+          Domeneack Foster
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/domeneack/react-weather-app"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
-
-
