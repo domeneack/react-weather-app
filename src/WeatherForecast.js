8ba1e83ca3d3e129ca2a5cf7861f16498bab15db
domeneack <112799387+domeneack@users.noreply.github.com>
2023-05-01 14:10:49 -0700

    Added coordinates api call

---
+++ src/WeatherForecast.js
@@ -0,0 +1,32 @@
+import React from "react";
+import WeatherIcon from "./WeatherIcon";
+import "./WeatherForecast.css";
+import axios from "axios";
+
+export default function WeatherForecast(props) {
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+
+  let apiKey = "57821c3b75b60c68ecd1a8d0dd1aa8d3";
+  let longitude = props.coordinates.lon;
+  let latitude = props.coordinates.lat;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(handleResponse);
+    
+  return (
+    <div className="WeatherForecast">
+      <div className="row">
+        <div className="col">
+          <div className="weatherForecast-day">Thu</div>
+          <WeatherIcon code="01d" size={36} />
+          <div className="WeatherForecast-temperature">
+            <span className="WeatherForecast-temperature-max">19º</span>
+            <span className="WeatherForecast-temperature-min">10º</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
