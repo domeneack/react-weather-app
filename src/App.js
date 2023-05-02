db53a87f9680875ff262417f7b3fbdc049f2176a
domeneack <112799387+domeneack@users.noreply.github.com>
2023-05-02 00:08:38 -0700

    Added target=_blank to gitHub, portfolio, and hosting link

--- src/App.js
+++ src/App.js
@@ -8,15 +8,27 @@ export default function App() {
         <Weather defaultCity="New York" />
         <footer className="footer">
           This project was coded by{" "}
-          <a href="https://tangerine-moonbeam-52db1f.netlify.app/">
+          <a
+            href="https://tangerine-moonbeam-52db1f.netlify.app/"
+            target="_blank"
+            rel="true"
+          >
             Domeneack Foster
           </a>{" "}
           and is{" "}
-          <a href="https://github.com/domeneack/react-weather-app">
+          <a
+            href="https://github.com/domeneack/react-weather-app"
+            target="_blank"
+            rel="true"
+          >
             open-sourced on GitHub
           </a>{" "}
           and{" "}
-          <a href="https://willowy-squirrel-9a862d.netlify.app/">
+          <a
+            href="https://willowy-squirrel-9a862d.netlify.app/"
+            target="_blank"
+            rel="true"
+          >
             hosted on Netlify
           </a>
         </footer>
